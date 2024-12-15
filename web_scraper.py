86411a3bdf3b1b18013827d13fb2422e4c286f7a
Doncho Ivanov <divanov@asteasolutions.com>
2024-12-15 19:25:03 +0200

    make it func

--- web_scraper.py
+++ web_scraper.py
@@ -2,44 +2,44 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-search_term = input("Какъв продукт търсиш? ")
-fixed_search_term = str(search_term).replace(' ', '+')
+def scrape(search_term):
+	fixed_search_term = str(search_term).replace(' ', '+')
 
-url = f"https://www.newegg.ca/p/pl?d={fixed_search_term}&N=4131"
-page = requests.get(url).text
-doc = BeautifulSoup(page, 'html.parser')
+	url = f"https://www.newegg.ca/p/pl?d={fixed_search_term}&N=4131"
+	page = requests.get(url).text
+	doc = BeautifulSoup(page, 'html.parser')
 
-page_text = doc.find(class_="list-tool-pagination-text").strong
-pages = int(str(page_text).split('/')[-2].split(">")[-1][:-1])
+	page_text = doc.find(class_="list-tool-pagination-text").strong
+	pages = int(str(page_text).split('/')[-2].split(">")[-1][:-1])
 
-items_found = {}
+	items_found = {}
 
-for page in range(1, pages + 1):
-    url = f"https://www.newegg.ca/p/pl?d={fixed_search_term}&N=4131&page={page}"
-    page = requests.get(url).text
-    doc = BeautifulSoup(page, "html.parser")
-    
-    div = doc.find(class_="item-cells-wrap border-cells short-video-box items-list-view is-list")
+	for page in range(1, pages + 1):
+		url = f"https://www.newegg.ca/p/pl?d={fixed_search_term}&N=4131&page={page}"
+		page = requests.get(url).text
+		doc = BeautifulSoup(page, "html.parser")
+		
+		div = doc.find(class_="item-cells-wrap border-cells short-video-box items-list-view is-list")
 
-    items = div.find_all(string=re.compile(search_term, re.IGNORECASE))
-    
-    for item in items:
-        parent = item.parent
-        if parent.name != "a":
-            continue
-        
-        link = parent['href']
-        next_parent = item.find_parent(class_="item-container")
-        try:
-            price = next_parent.find(class_="price-current").find("strong").string
-            items_found[item] = {"price": int(price.replace(",", "")), "link": link}
-        except:
-            pass
+		items = div.find_all(string=re.compile(search_term, re.IGNORECASE))
+		
+		for item in items:
+			parent = item.parent
+			if parent.name != "a":
+				continue
+			
+			link = parent['href']
+			next_parent = item.find_parent(class_="item-container")
+			try:
+				price = next_parent.find(class_="price-current").find("strong").string
+				items_found[item] = {"price": int(price.replace(",", "")), "link": link}
+			except:
+				pass
 
-sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
+	sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
 
-for item in sorted_items:
-	print(f"Продукт: {item[0]}")
-	print(f"Цена: ${item[1]['price']}")
-	print(item[1]['link'])
-	print("-------------------------------")
+	for item in sorted_items:
+		print(f"Продукт: {item[0]}")
+		print(f"Цена: ${item[1]['price']}")
+		print(item[1]['link'])
+		print("-------------------------------")
